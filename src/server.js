ab38e09e265ed62fb2de52b914093a7969f9d5d0
Fernando Mariano Pereira <fernandovmp@outlook.com>
2019-08-18 14:30:53 -0300

    Adição do CORS.

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const routes = require('./routes');
 const moongose = require('mongoose');
+const cors = require('cors');
 require('dotenv/config');
 
 const app = express();
@@ -12,6 +13,8 @@ moongose.connect(connectionString,
 {
    useNewUrlParser: true
 });
+
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 
