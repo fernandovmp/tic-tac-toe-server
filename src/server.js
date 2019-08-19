b90e756a8d9dd1121e2ddbac4f6e1c9d6a74048d
Fernando Mariano Pereira <fernandovmp@outlook.com>
2019-08-19 18:28:27 -0300

    Permisão para uso de credênciais.

--- src/server.js
+++ src/server.js
@@ -14,7 +14,10 @@ moongose.connect(connectionString,
    useNewUrlParser: true
 });
 
-app.use(cors());
+app.use(cors({
+   origin: 'http://localhost:3001',
+   credentials: true
+}));
 app.use(express.json());
 app.use(cookieParser());
 app.use(routes);
