90b1b035d9f405be07ad7da3898252b5030d2f35
Fernando Mariano Pereira <fernandovmp@outlook.com>
2019-08-20 17:52:29 -0300

    Troca da porta padrão e do enderço de origem permitido pelo CORS.

--- src/server.js
+++ src/server.js
@@ -6,7 +6,7 @@ require('dotenv/config');
 const cookieParser = require('cookie-parser');
 const app = express();
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3001;
 const connectionString = process.env.CONNECTION_STRING;
 
 moongose.connect(connectionString,
@@ -15,7 +15,7 @@ moongose.connect(connectionString,
 });
 
 app.use(cors({
-   origin: 'http://localhost:3001',
+   origin: 'http://localhost:3000',
    credentials: true
 }));
 app.use(express.json());
