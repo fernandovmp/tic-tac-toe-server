93af66c862f37aa85b139a796bd3f06388f61646
Fernando Mariano Pereira <fernandovmp@outlook.com>
2019-08-23 19:21:44 -0300

    Configuração de socket.io

--- src/server.js
+++ src/server.js
@@ -4,7 +4,18 @@ const moongose = require('mongoose');
 const cors = require('cors');
 require('dotenv/config');
 const cookieParser = require('cookie-parser');
+
 const app = express();
+
+const server = require('http').Server(app);
+
+const connectedUsers = {};
+
+const io = require('socket.io')(server);
+io.on('connection', socket => {
+   const { user } = socket.handshake.query;
+   connectedUsers[user] = socket.id;
+});
 
 const port = process.env.PORT || 3001;
 const connectionString = process.env.CONNECTION_STRING;
@@ -14,6 +25,11 @@ moongose.connect(connectionString,
    useNewUrlParser: true
 });
 
+app.use((req, res, next) => {
+   req.io = io;
+   req.connectedUsers = connectedUsers;
+   next();
+});
 app.use(cors({
    origin: 'http://localhost:3000',
    credentials: true
@@ -22,6 +38,6 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(routes);
 
-app.listen(port, () => {
+server.listen(port, () => {
    console.log(`Server running on port ${port}`); 
 });
